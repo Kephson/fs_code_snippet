540389a7cffd5600d925bb943a57a2cb87e9f43c
Ephraim Härer <ephraim.haerer@renolit.com>
2025-11-10 10:53:11 +0100

    disabled non-existing hook

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,8 +8,8 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 call_user_func(
     function () {
         // Register for hook to show a preview of elements of CType="fs_code_snippet" in page module
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['fs_code_snippet']
-            = \DanielGoerz\FsCodeSnippet\Hook\CodeSnippetPreviewRenderer::class;
+        /*$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['fs_code_snippet']
+            = \DanielGoerz\FsCodeSnippet\Hook\CodeSnippetPreviewRenderer::class;*/
 
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1449747562] = [
             'nodeName' => 'fs_code_snippet',
